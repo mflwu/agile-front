021f838cc467ddafa67bc68b36e4cc6f462f15e1
Youssef <youschaouki@gmail.com>
2024-12-13 11:03:18 +0100

    fix

--- src/api/simpleRequests.jsx
+++ src/api/simpleRequests.jsx
@@ -7,7 +7,7 @@ export const sendRequestToBackend = async (updatedRequest) => {
             deliveryId: updatedRequest.delivery.id,
         };
 
-        const response = await fetch("http://localhost:8080/delivery/requests", {
+        const response = await fetch("http://localhost:8080/city-map/delivery/requests", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -26,26 +26,5 @@ export const sendRequestToBackend = async (updatedRequest) => {
     } catch (error) {
         console.error("Error sending request to backend:", error);
         throw error; // Re-throw to allow caller to handle it
-    }
-};
-
-export const fetchRouteCoordinates = async (nodeIds) => {
-    try {
-        const response = await fetch("http://localhost:8080/route-coordinates", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(nodeIds),
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to fetch route coordinates");
-        }
-
-        return response.json(); // Returns a list of coordinates
-    } catch (error) {
-        console.error("Error fetching route coordinates:", error);
-        throw error;
     }
 };
