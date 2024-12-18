bfed3b0b622b6dbf42d65f4da1ff6626fb24ba7a
Youssef <youschaouki@gmail.com>
2024-12-18 17:43:44 +0100

    uploading the

--- src/api/Services.jsx
+++ src/api/Services.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+
 const API_URL = "http://localhost:8080";
 
 const apiClient = axios.create({
@@ -34,4 +35,22 @@ export async function fetchIntersections() {
         throw error;
     }
 }
+
+// Importer un fichier XML pour traitement
+export async function importXMLFile(fileName) {
+    try {
+        const payload = { filePath: fileName }; // Assure-toi que la clé correspond à ce que le back-end attend
+
+        const response = await apiClient.post(`/import-xml`, payload);
+
+        console.log("File name sent successfully:", response.data);
+        return response.data;
+    } catch (error) {
+        console.error("Error sending file name:", error.response?.data || error.message);
+        throw error;
+    }
+}
+
+
+
 
